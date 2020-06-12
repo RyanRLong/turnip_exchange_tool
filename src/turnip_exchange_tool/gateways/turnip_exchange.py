58de771c2cc8a401a1b8e591a6db3e5afe7126b7
Ryan Long <ryanlong1004@gmail.com>
2020-06-11 20:54:04 -0400

    Add Brotli for br compression

--- src/turnip_exchange_tool/gateways/turnip_exchange.py
+++ src/turnip_exchange_tool/gateways/turnip_exchange.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 import pickle
+import brotli
 
 import requests
 
@@ -23,7 +24,7 @@ def request_data(file_path=None):
         response = _make_request()
     log.debug(f"Response Headers: {response.headers}")
     log.debug(f"Response Text: {response.text}")
-    return json.loads(response.text)
+    return json.loads(brotli.decompress(response.text))
 
 
 def _make_request():
